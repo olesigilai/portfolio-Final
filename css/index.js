5f11f63ed8067d43a3a52fc0aad5c91257f11e31
olesigilai <ole_sigilai@yahoo.com>
2021-05-10 10:53:07 +0300

    js content for scroll is added to allow smooth scroll of the content

--- css/index.js
+++ css/index.js
@@ -11,67 +11,67 @@ const showMenu = (toggleId, navId) =>{
 }
 showMenu('nav-toggle','nav-menu')
 
-// /*===== REMOVE MENU MOBILE =====*/
-// const navLink = document.querySelectorAll('.nav__link')
 
-// function linkAction(){
-//     const navMenu = document.getElementById('nav-menu')
-//     navMenu.classList.remove('show')
-// }
-// navLink.forEach(n => n.addEventListener('click', linkAction))
+const navLink = document.querySelectorAll('.nav__link')
 
-// /*===== SCROLL SECTIONS ACTIVE LINK =====*/
-// const sections = document.querySelectorAll('section[id]')
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
-// window.addEventListener('scroll', scrollActive)
+/*===== SCROLL SECTIONS ACTIVE LINK =====*/
+const sections = document.querySelectorAll('section[id]')
 
-// function scrollActive(){
-//     const scrollY = window.pageYOffset
+window.addEventListener('scroll', scrollActive)
 
-//     sections.forEach(current =>{
-//         const sectionHeight = current.offsetHeight
-//         const sectionTop = current.offsetTop - 50;
-//         sectionId = current.getAttribute('id')
+function scrollActive(){
+    const scrollY = window.pageYOffset
 
-//         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-//             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
-//         }else{
-//             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
-//         }
-//     })
-// }
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
 
-// /*===== SCROLL REVEAL ANIMATION =====*/
-// const sr = ScrollReveal({
-//     origin: 'top',
-//     distance: '80px',
-//     duration: 2000,
-//     reset: true
-// })
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
 
-// /*SCROLL HOME*/
-// sr.reveal('.home__title', {})
-// sr.reveal('.home__scroll', {delay: 200})
-// sr.reveal('.home__img', {origin:'right', delay: 400})
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
 
-// /*SCROLL ABOUT*/
-// sr.reveal('.about__img', {delay: 500})
-// sr.reveal('.about__subtitle', {delay: 300})
-// sr.reveal('.about__profession', {delay: 400})
-// sr.reveal('.about__text', {delay: 500})
-// sr.reveal('.about__social-icon', {delay: 600, interval: 200})
+/*SCROLL HOME*/
+sr.reveal('.home__title', {})
+sr.reveal('.home__scroll', {delay: 200})
+sr.reveal('.home__img', {origin:'right', delay: 400})
 
-// /*SCROLL SKILLS*/
-// sr.reveal('.skills__subtitle', {})
-// sr.reveal('.skills__name', {distance: '20px', delay: 50, interval: 100})
-// sr.reveal('.skills__img', {delay: 400})
+/*SCROLL ABOUT*/
+sr.reveal('.about__img', {delay: 500})
+sr.reveal('.about__subtitle', {delay: 300})
+sr.reveal('.about__profession', {delay: 400})
+sr.reveal('.about__text', {delay: 500})
+sr.reveal('.about__social-icon', {delay: 600, interval: 200})
 
-// /*SCROLL PORTFOLIO*/
-// sr.reveal('.portfolio__img', {interval: 200})
+/*SCROLL SKILLS*/
+sr.reveal('.skills__subtitle', {})
+sr.reveal('.skills__name', {distance: '20px', delay: 50, interval: 100})
+sr.reveal('.skills__img', {delay: 400})
 
-// /*SCROLL CONTACT*/
-// sr.reveal('.contact__subtitle', {})
-// sr.reveal('.contact__text', {interval: 200})
-// sr.reveal('.contact__input', {delay: 400})
-// sr.reveal('.contact__button', {delay: 600})
+/*SCROLL PORTFOLIO*/
+sr.reveal('.portfolio__img', {interval: 200})
+
+/*SCROLL CONTACT*/
+sr.reveal('.contact__subtitle', {})
+sr.reveal('.contact__text', {interval: 200})
+sr.reveal('.contact__input', {delay: 400})
+sr.reveal('.contact__button', {delay: 600})
 
